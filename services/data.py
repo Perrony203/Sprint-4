99b965e380ef8e8c7683c9aa95a611402387efa2
vrtx <jose.gomez4@eia.edu.co>
2025-06-08 18:04:27 -0500

    Finished Sprint 6?

--- services/data.py
+++ services/data.py
@@ -1,7 +1,13 @@
 from models.dueño import Dueño
 from models.consulta import Consulta
 from models.mascota import Mascota
+from util.serializador import cargar_mascotas_dueños, cargar_consultas
+
 # Base de datos temporal de los objetos requeridos para la aplicación
 dueños: list[Dueño] = []
 mascotas: list[Mascota] = []
 consultas: list[Consulta] = []
+
+# Cargar datos al inicio
+mascotas, dueños = cargar_mascotas_dueños()
+consultas = cargar_consultas(mascotas)
